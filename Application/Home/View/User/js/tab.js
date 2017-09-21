d2b605338fb7528b6e6e5f8ed8faf159dd12a18b
1072357480 <1072357480@qq.com>
2017-09-21 17:15:39 +0800

    s

--- Application/Home/View/User/js/tab.js
+++ Application/Home/View/User/js/tab.js
@@ -12,7 +12,7 @@ $(function(){
 			$(this).addClass("edit_on").siblings('li').removeClass("edit_on");
 			var index=$(this).index();
 			$(".ch_edit .duration").eq(index).addClass('dis_block').show().siblings().removeClass('dis_block').hide();
-			$(".dis_block li:first-child").addClass('edit_on');
+			$(".dis_block li:first-child").addClass('edit_on').siblings('li').removeClass('edit_on');
 			aihuoma();
 		})
 	})
@@ -33,7 +33,7 @@ $(function(){
 	function aihuoma(){
 		var banben=$(".edition .edit_on").html();
 		var nian=$(".duration.dis_block .edit_on p").html();
-		var nian_num=nian.split('年')[0];
+		var nian_num=nian.split('')[0];
 		var edu=$(".duration.dis_block .edit_on .jine").html();
 		var tot=nian_num*edu;
 		$(".col1").html(banben+nian);
