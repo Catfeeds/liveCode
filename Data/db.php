fbb8e95a4c7cc1a811fd73758d5f180d82123b1e
1072357480 <1072357480@qq.com>
2017-09-25 08:55:47 +0800

    22

--- Data/db.php
+++ Data/db.php
@@ -20,7 +20,7 @@
         'DB_HOST'   =>'127.0.0.1',       // 服务器地址
         'DB_NAME'   =>'livecode',       // 数据库名
         'DB_USER'   =>'root',       // 用户名
-        'DB_PWD'    =>'',        // 密码
+        'DB_PWD'    =>'root',        // 密码
         'DB_PORT'   =>'3306',            // 端口
         'DB_PREFIX' =>'fw_',   // 数据库表前缀
     );
