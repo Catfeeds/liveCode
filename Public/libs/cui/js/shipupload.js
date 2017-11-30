19bfa601a641e106388f986908ce76e1b435ea6a
1072357480 <1072357480@qq.com>
2017-11-30 09:44:40 +0800

    f

---
+++ Public/libs/cui/js/shipupload.js
@@ -0,0 +1,30 @@
+$(function(){
+	var postUrl = 'admin.php?s=/admin/livecode/addfile';
+	var codeType= $("#codeType").val();     //管理员审核编辑
+    if (codeType) {
+        postUrl = 'admin.php?s=/admin/checkcode/addLivecodeFile';
+    }
+    var productPostUrl = 'admin.php?s=/admin/product/addfile';
+	var codeType= $("#codeType").val();     //管理员审核编辑
+    if (codeType) {
+        productPostUrl = 'admin.php?s=/admin/product/addLivecodeFile';
+    }
+
+	//文件活码上传
+	layui.config({
+    base:'/shipath/', //mupload模块所在的目录
+	}).use(['mupload'], function(){
+        layui.mupload({
+          //必须，服务器路径
+          url:postUrl,
+          //必须，传递table的id,请不要传class
+          container:'#shiptab',
+          //默认"file"，用于服务器获取文件名
+          file_name:'file',
+          //回调函数
+          complete:function(res){
+              console.log(res)
+          }
+	    });
+	})
+})
